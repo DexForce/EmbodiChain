c607219c787a8160833a5fdab5257ae5dd6a796e
chenjian <chenjian@dexforce.com>
2025-12-05 18:08:28 +0800

    revert

--- embodichain/lab/sim/objects/gizmo.py
+++ embodichain/lab/sim/objects/gizmo.py
@@ -40,7 +40,7 @@ from dexsim.types import (
     RigidBodyShape,
     PhysicalAttr,
 )
-from dexsim.engine import GizmoController
+from dexsim.render import GizmoController
 
 from embodichain.lab.sim.utility.gizmo_utils import create_gizmo_callback
 
