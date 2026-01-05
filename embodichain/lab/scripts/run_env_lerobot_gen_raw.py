0c16216e80a90050ce6ef458c70b57fc7e2eb0d9
chenjian <chenjian@dexforce.com>
2026-01-05 11:54:12 +0800

    update

--- embodichain/lab/scripts/run_env_lerobot_gen_raw.py
+++ embodichain/lab/scripts/run_env_lerobot_gen_raw.py
@@ -56,8 +56,6 @@ def generate_and_execute_action_list(env, idx, debug_mode):
 
 def generate_function(
     env,
-    num_traj,
-    time_id: int = 0,
     save_path: str = "",
     save_video: bool = False,
     debug_mode: bool = False,
@@ -83,35 +81,18 @@ def generate_function(
     """
 
     valid = True
-    while True:
+    n_traj = len(env.trajectory_list)
+
+    for trajectory_idx in range(n_traj):
         _, _ = env.reset()
+        valid = generate_and_execute_action_list(env, trajectory_idx, debug_mode)
 
-        ret = []
-        for trajectory_idx in range(num_traj):
-            valid = generate_and_execute_action_list(env, trajectory_idx, debug_mode)
-
-            if not valid:
-                break
-
-            if not debug_mode and env.is_task_success().item():
-                dataset_id = f"time_{time_id}_trajectory_{trajectory_idx}"
-                data_dict = env.to_dataset(
-                    repo_id=dataset_id,
-                )
-                ret.append(data_dict)
-
-                # TODO: Add data saving and online data streaming logic here.
-
-            else:
-                log_warning(f"Task fail, Skip to next generation.")
-                valid = False
-                break
-
-        if valid:
+        if not valid:
             break
-        else:
-            log_warning("Reset valid flag to True.")
-            valid = True
+
+        if not debug_mode:
+            dataset_id = f"trajectory_{trajectory_idx}"
+            data_dict = env.to_dataset()
 
     return True
 
@@ -120,17 +101,12 @@ def main(args, env, gym_config):
 
     log_info("Start offline data generation.", color="green")
     # TODO: Support multiple trajectories per episode generation.
-    num_traj = 1
-    # run env with maximum trajectory number
-    for i in range(env.n_trajectory):
-        generate_function(
-            env,
-            num_traj,
-            i,
-            save_path=args.save_path,
-            save_video=args.save_video,
-            debug_mode=args.debug_mode,
-        )
+    generate_function(
+        env,
+        save_path=args.save_path,
+        save_video=args.save_video,
+        debug_mode=args.debug_mode,
+    )
 
 
 if __name__ == "__main__":
@@ -194,8 +170,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args.num_envs != 1:
-        log_error(f"Currently only support num_envs=1, but got {args.num_envs}.")
+    # if args.num_envs != 1:
+    #     log_error(f"Currently only support num_envs=1, but got {args.num_envs}.")
 
     gym_config = load_json(args.gym_config)
     cfg: EmbodiedEnvCfg = config_to_cfg(gym_config)
