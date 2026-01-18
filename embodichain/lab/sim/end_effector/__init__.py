2ede9a078346234ec45937746683d4f664a51954
yangchen73 <122090643@link.cuhk.edu.cn>
2026-01-18 18:18:22 +0800

    Migrate end effector

---
+++ embodichain/lab/sim/end_effector/__init__.py
@@ -0,0 +1,9 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2021-2025 DexForce Technology Co., Ltd.
+#
+# All rights reserved.
+# ----------------------------------------------------------------------------
+from .end_effector import EndEffector
+from .utility import *
+
+del end_effector, utility
