029da7afd1665306e4ca879df265da490cea98ba
chenjian <chenjian@dexforce.com>
2026-01-27 14:48:13 +0800

    update

--- embodichain/lab/sim/cfg.py
+++ embodichain/lab/sim/cfg.py
@@ -393,6 +393,7 @@ class JointDrivePropertiesCfg:
 
     If the drive type is "force", then the joint is driven by a force and the acceleration is computed based on the force applied.
     If the drive type is "acceleration", then the joint is driven by an acceleration and the force is computed based on the acceleration applied.
+    If the drive type is "none", then no force will be applied to joint.
     """
 
     stiffness: Union[Dict[str, float], float] = 1e4
