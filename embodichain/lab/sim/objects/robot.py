fa1e2b14f42ce992e0ab60c658335da81ce6be84
chenjian <chenjian@dexforce.com>
2026-04-08 12:16:48 +0800

    update

--- embodichain/lab/sim/objects/robot.py
+++ embodichain/lab/sim/objects/robot.py
@@ -498,7 +498,12 @@ class Robot(Articulation):
                 f"Joint positions shape mismatch. Expected {solver.dof} joints, got {qpos.shape[1]}."
             )
 
-        result_matrix = solver.get_fk(qpos=qpos)
+        if qpos.device != self.device:
+            qpos_ = qpos.to(self.device)
+        else:
+            qpos_ = qpos
+
+        result_matrix = solver.get_fk(qpos=qpos_)
 
         base_pose = self.get_link_pose(
             link_name=solver.root_link_name, env_ids=local_env_ids, to_matrix=True
@@ -633,8 +638,13 @@ class Robot(Articulation):
                 f"Joint positions shape mismatch. Expected {solver.dof} joints, got {qpos.shape[1]}."
             )
 
-        n_batch = qpos.shape[1]
-        qpos_batch = qpos.reshape(-1, solver.dof)
+        if qpos.device != self.device:
+            qpos_ = qpos.to(self.device)
+        else:
+            qpos_ = qpos
+
+        n_batch = qpos_.shape[1]
+        qpos_batch = qpos_.reshape(-1, solver.dof)
         xpos_batch = solver.get_fk(qpos=qpos_batch)
 
         # get xpos from link root
